0eb0debc56fe2f6862c661ab41b78db618d9941f
prem <praulayar994@gmail.com>
2023-12-23 23:03:48 +0530

    connectdb and utils file

---
+++ src/dbConfig/connectDB.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Database connection established");
+        console.log(`DB_Host : ${connInstance.connection.host}`)
+        console.log("-------------------------------------------");
+    } catch (err) {
+        console.log("-------------------------------------------");
+        console.log(`Internal Server Error while connecting to database --> ${err}!!!`);
+        process.exit(1);
+    }
+}
+
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import app from "./app.js";
+import connectDB from "./dbConfig/connectDB.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`App couldn't connect to database: ${e.message}`);
+      throw e
+    });
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
